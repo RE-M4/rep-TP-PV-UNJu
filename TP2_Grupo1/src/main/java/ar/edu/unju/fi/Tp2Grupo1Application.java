f00b77662dc9141731031ce0e5af48769c5735fa
RE-M4 <evilmlr2018@gmail.com>
2023-04-18 20:55:41 -0300

    Commit de prueba.

--- TP2_Grupo1/src/main/java/ar/edu/unju/fi/Tp2Grupo1Application.java
+++ TP2_Grupo1/src/main/java/ar/edu/unju/fi/Tp2Grupo1Application.java
@@ -8,7 +8,7 @@ public class Tp2Grupo1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Tp2Grupo1Application.class, args);
-		System.out.println("Test");
+		System.out.println("Test 2");
 	}
 
 }
