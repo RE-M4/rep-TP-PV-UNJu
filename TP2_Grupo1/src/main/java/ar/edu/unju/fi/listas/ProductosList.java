31f8bb15faf6847c14fd26e70ba198f12b4faea4
DESKTOP-OQ78GJK\Elias <elias3847040@gmail.com>
2023-05-21 20:24:21 -0300

    Se actuliza la logica de productos para que funcione de forma dinamica

--- TP2_Grupo1/src/main/java/ar/edu/unju/fi/listas/ProductosList.java
+++ TP2_Grupo1/src/main/java/ar/edu/unju/fi/listas/ProductosList.java
@@ -10,8 +10,9 @@ public class ProductosList {
 	
 	public ProductosList() {
 		productos = new ArrayList<Producto>();
-		productos.add(new Producto("OLYMPIC PERRO ADULTO 19KG","Alimento canino",0001,50,5750.00));
-		productos.add(new Producto("COMPLETE PERRO CACHORRO RAZA PEQUEÑA 20 KG","Alimento canino cachorro",0002,25,10837.00));
+		productos.add(new Producto("OLYMPIC PERRO ADULTO 19KG","Alimento canino",50,5750.00,"../images/producto1.jpg",1));
+		productos.add(new Producto("COMPLETE PERRO CACHORRO RAZA PEQUEÑA 20 KG","Alimento canino cachorro",25,10837.00,"../images/producto2.jpg",2));
+		productos.add(new Producto("OLYMPIC PERRO ADULTO 19KG","Alimento canino",0,5750.00,"../images/producto1.jpg",3));
 	}
 	
 	public List<Producto> getProductos() {
