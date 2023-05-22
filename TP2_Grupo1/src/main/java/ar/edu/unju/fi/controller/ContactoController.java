cb8dc045c75fc4f2cc02cb6cc9393dbd2f71f0c9
Javier <Javier@DESKTOP-6AIS6L0>
2023-05-21 22:44:59 -0300

    Resolución

--- TP2_Grupo1/src/main/java/ar/edu/unju/fi/controller/ContactoController.java
+++ TP2_Grupo1/src/main/java/ar/edu/unju/fi/controller/ContactoController.java
@@ -3,7 +3,9 @@ package ar.edu.unju.fi.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import ar.edu.unju.fi.listas.ListaContacto;
 
@@ -11,26 +13,43 @@ import ar.edu.unju.fi.controller.ContactoController;
 import ar.edu.unju.fi.model.Contacto;
 
 @Controller
+@RequestMapping("/contacto")
 public class ContactoController {
 	
 	ListaContacto listaMensajes = new ListaContacto();
 	
-	@GetMapping("/contacto")
+	@GetMapping("/listaMensaje")
 	public String contacto(Model model) {
-		model.addAttribute("Contacto", new Contacto());
-		return "contacto";
+		model.addAttribute("contacto", listaMensajes.getListaMensaje());
+		return "listaMensaje";
 	}
 	
 	@PostMapping("/guardarMensaje")
 	public String guardar(Model model , Contacto contacto) {
 		listaMensajes.getListaMensaje().add(contacto); 
 		model.addAttribute("listaMensajes" , listaMensajes.getListaMensaje());
-		console.log("asdasdsa");
-    
     return "listaMensaje";
 
 	}
+	/*@GetMapping("/eliminarMensaje/{nombreMensaje}")
+	public String eliminarMensaje(Model model, @PathVariable(value="nombreMensaje")String nombreMensaje) {
+	Contacto mensajeEncontrado = new Contacto();
+	for(Contacto contacto : listaMensajes.getListaMensaje()) {
+	if(contacto.getNombre().equals(nombreMensaje)) {
+		mensajeEncontrado = contacto;
+		mensajeEncontrado.setNombre("");
+		mensajeEncontrado.setEmail("");
+		mensajeEncontrado.setCiudad("");
+		mensajeEncontrado.setMensaje("");
+		break;
+	}	
+	}
+	return "redirect:/contacto/listaMensaje";
+	}
+	*/
 
-}
+		
+}	
+
 
 
