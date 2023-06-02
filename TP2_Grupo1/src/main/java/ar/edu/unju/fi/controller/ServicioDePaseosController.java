677bf1617827428c356b5538a5578356fb7882af
RE-M4 <evilmlr2018@gmail.com>
2023-06-02 18:18:40 -0300

    Actualización de Servicios de Paseo, Index y Barra de navegación
    Se implementa Bootstrap en index.html para que sea responsiva.
    Se actualiza navigation.html para que aplique propiedades de Bootstrap
    para que sea responsiva de forma adecuada.
    Se implementa el concepto de inyección de dependencias en
    ServicioDePaseo.java; ServicioDePaseoLista.java y el controller
    correspondiente.
    Se corrige el funcionamiento de la opción "Eliminar" de la tabla de
    servicioDePaseos.html.
    Se implementa el concepto de validaciones en ServicioDePaseo.java y
    modificar_servicios.html.

--- TP2_Grupo1/src/main/java/ar/edu/unju/fi/controller/ServicioDePaseosController.java
+++ TP2_Grupo1/src/main/java/ar/edu/unju/fi/controller/ServicioDePaseosController.java
@@ -1,12 +1,15 @@
 package ar.edu.unju.fi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.unju.fi.listas.ServiciosDePaseoLista;
 import ar.edu.unju.fi.model.ServicioDePaseo;
+import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -17,10 +20,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RequestMapping("/servicioDePaseos") /*Hace falta esta línea para no mezclar los GetMapping o PostMapping de otros Controllers*/
 public class ServicioDePaseosController {
 	/*Se crea un objeto tipo lista*/
-	ServiciosDePaseoLista serviciosLista = new ServiciosDePaseoLista();
+	@Autowired
+	private ServiciosDePaseoLista serviciosLista;
 	/*Se crea una variable controladora para saber que función tendrá el formulario cuando se lo invoque*/
 	Boolean edicion = false;
 	/*GetMapping que usa el método "get" del objeto tipo lista y lo carga en el HTML*/
+	@Autowired
+	private ServicioDePaseo servicioDePaseo;
 	@GetMapping("/listado")
 	public String getServicioDePaseos(Model model) {
 		model.addAttribute("servicios", serviciosLista.getServiciosDePaseo());
@@ -28,7 +34,9 @@ public class ServicioDePaseosController {
 	}
 	@GetMapping("/modificarDatos/{nombrePaseador}")
 	public String getModificarDatos(Model model, @PathVariable(value="nombrePaseador")String nombrePaseador) { /*PathVariable se obtiene de la línea 28 de servicioDePaseos.html*/
-		ServicioDePaseo servicioEncontrado = new ServicioDePaseo();
+		edicion = true;
+		System.out.println(edicion);
+		ServicioDePaseo servicioEncontrado = servicioDePaseo;
 		for(ServicioDePaseo servicio : serviciosLista.getServiciosDePaseo()) {/*Se busca un objeto dentro del Array tienendo en cuenta el nombre*/
 			if(servicio.getPaseador().equals(nombrePaseador)) {
 				servicioEncontrado = servicio;
@@ -40,8 +48,12 @@ public class ServicioDePaseosController {
 		return "modificar_servicios";
 	}
 	@PostMapping("/modificarDatos")
-	public String modificarDatos(@ModelAttribute("ServicioDePaseo")ServicioDePaseo servicioModificado) {/*ModelAttribute sirve para identificar un objeto a modificar | ServicioDePaseo (o sea, la etiqueta servicioModificado) es el th:object de modificar_servicios.html con sus atributos cambiados al momento de cambiar los inputs y pulsar "Guardar"*/
-		edicion = true;
+	public String modificarDatos(@Valid @ModelAttribute("ServicioDePaseo")ServicioDePaseo servicioModificado, BindingResult result, Model model) {/*ModelAttribute sirve para identificar un objeto a modificar | ServicioDePaseo (o sea, la etiqueta servicioModificado) es el th:object de modificar_servicios.html con sus atributos cambiados al momento de cambiar los inputs y pulsar "Guardar"*/
+		System.out.println(edicion);
+		if(result.hasErrors()) {
+			model.addAttribute("ServicioDePaseo", servicioModificado);
+			return "modificar_servicios";
+		}
 		for(ServicioDePaseo servicioOriginal : serviciosLista.getServiciosDePaseo()) {
 			if(servicioOriginal.getPaseador().equals(servicioModificado.getPaseador())) {
 				//servicioOriginal = servicioModificado /*Esta línea no modifica los datos de un objeto con otro objeto, solo crea 2 punteros al mismo objeto*/
@@ -54,13 +66,11 @@ public class ServicioDePaseosController {
 	}
 	@GetMapping("/eliminarDatos/{nombrePaseador}")
 	public String eliminarDatos(Model model, @PathVariable(value="nombrePaseador")String nombrePaseador){
-		ServicioDePaseo servicioEncontrado = new ServicioDePaseo();
+		ServicioDePaseo servicioEncontrado = servicioDePaseo;
 		for(ServicioDePaseo servicio : serviciosLista.getServiciosDePaseo()) {
 			if(servicio.getPaseador().equals(nombrePaseador)) {
 				servicioEncontrado = servicio;
-				servicioEncontrado.setPaseador("");
-				servicioEncontrado.setDia("");
-				servicioEncontrado.setHorario("");
+				serviciosLista.getServiciosDePaseo().remove(servicioEncontrado);
 				break;
 			}
 		}
@@ -68,15 +78,23 @@ public class ServicioDePaseosController {
 	}
 	@GetMapping("/nuevoServicio")
 	public String nuevaSucursal(Model model) {
-		model.addAttribute("ServicioDePaseo", new ServicioDePaseo());//Se envia un nuevo objeto "ServicioDePaseo" que será el th:object de modificar_servicios.html.
+		edicion = false;
+		System.out.println(edicion);
+		model.addAttribute("ServicioDePaseo", servicioDePaseo);//Se envia un nuevo objeto "ServicioDePaseo" que será el th:object de modificar_servicios.html.
 		model.addAttribute("edicion", edicion);
 		return "modificar_servicios";
 	}
 	@PostMapping("/guardarDato")
-	public String guardarNuevoDato(@ModelAttribute("ServicioDePaseo")ServicioDePaseo nuevoServicio) {
-		//ModelAndView modelAndView = new ModelAndView("servicioDePaseos"); Se crea un objeto ModelAndView, lo que está entre comillas es la página HTML a la que se va a redirigir.
+	public ModelAndView guardarNuevoDato(@Valid @ModelAttribute("ServicioDePaseo")ServicioDePaseo nuevoServicio, BindingResult result) {
+		System.out.println(edicion);
+		ModelAndView modelAndView = new ModelAndView("redirect:/servicioDePaseos/listado"); //Se crea un objeto ModelAndView, lo que está entre comillas es la página HTML a la que se va a redirigir.
+		if(result.hasErrors()) {
+			modelAndView.setViewName("modificar_servicios");
+			modelAndView.addObject("ServicioDePaseo", nuevoServicio); //"ServicioDePaseo" tiene que coincidir con el th:object de modificar_servicios.html
+			return modelAndView;
+		}
 		serviciosLista.getServiciosDePaseo().add(nuevoServicio);
-		//modelAndView.addObject("servicioDePaseos", serviciosLista.getServiciosDePaseo());
-		return "redirect:/servicioDePaseos/listado";
+		modelAndView.addObject("serviciosDePaseo", serviciosLista.getServiciosDePaseo());
+		return modelAndView;
 	}
 }
