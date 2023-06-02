61063c9335183384f4f389c550b743a145bb9ec8
DESKTOP-OQ78GJK\Elias <elias3847040@gmail.com>
2023-06-02 02:57:08 -0300

    Se agrega Dependencias y validaciones para productos
    se convierte la clase producto en un componente y la clase listaproducto
    en componente
    
    Se agrega validaciones para el formulario que agrega nuevo productos en
    el template nuevo_producto

--- TP2_Grupo1/src/main/java/ar/edu/unju/fi/controller/ProductosController.java
+++ TP2_Grupo1/src/main/java/ar/edu/unju/fi/controller/ProductosController.java
@@ -1,9 +1,12 @@
 package ar.edu.unju.fi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 //import java.util.ArrayList;
 //import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 //import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,16 +16,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.unju.fi.listas.ProductosList;
-import ar.edu.unju.fi.model.ConsejoSalud;
 import ar.edu.unju.fi.model.Producto;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/producto")
 public class ProductosController {
 	
-	private ProductosList productos = new ProductosList();
+	@Autowired
+	private ProductosList productos;
 	
-	/**Este GetMapping utliza ModelAndView y usa un ArrayList de otra clase Java llamada ProductosList */
+	@Autowired
+	private Producto producto;
+	
+	/**
+	 * Este metodo nos devuelve
+	 * @return 
+	 */
 	@GetMapping("/productos")
 	public ModelAndView mostrarProductos() {
 		ModelAndView modelAndView = new ModelAndView("productos");
@@ -33,16 +43,23 @@ public class ProductosController {
 	@GetMapping("/nuevo_producto")
 	public ModelAndView getNuevoConsejo() {
 		ModelAndView modelAndView = new ModelAndView("nuevo_producto");
-		modelAndView.addObject("Producto", new Producto());
+		modelAndView.addObject("Producto", producto);
 		boolean edicion=false;
 		modelAndView.addObject("edicion", edicion);
 		return modelAndView;
 	}
 	@PostMapping("/guardar_producto")
-	public ModelAndView guardarConsejoSalud(@ModelAttribute("Producto") Producto producto) {
+	public ModelAndView guardarConsejoSalud(@Valid @ModelAttribute("Producto") Producto producto ,BindingResult result ) {
+		ModelAndView modelAndView = new ModelAndView("redirect:/producto/productos");
+		if(result.hasErrors()) {
+			modelAndView.setViewName("nuevo_producto");
+			modelAndView.addObject("Producto", producto);
+			boolean edicion=false;
+			modelAndView.addObject("edicion", edicion);
+			return modelAndView;
+		}
 		producto.setCodigo(productos.getProductos().get(productos.getProductos().size()-1).getCodigo()+1);  
 		productos.getProductos().add(producto);
-		ModelAndView modelAndView = new ModelAndView("redirect:/producto/productos");
 		modelAndView.addObject("productos", productos.getProductos());
 		return modelAndView;
 	}
@@ -50,7 +67,7 @@ public class ProductosController {
 	@GetMapping("/modificar_producto/{codigo}")
 	public ModelAndView getModificarConsejo(@PathVariable(value="codigo")Integer codigo) {
 		ModelAndView modelAndView = new ModelAndView("nuevo_producto");
-		Producto productoEncontrado = new Producto();
+		Producto productoEncontrado = producto;
 		for(Producto p: productos.getProductos()) {
 			if(p.getCodigo().equals(codigo)) {
 				productoEncontrado = p;
@@ -67,9 +84,18 @@ public class ProductosController {
 	}
 	
 	@PostMapping("/modificar_producto")
-	public ModelAndView modificarConsejoSalud(@ModelAttribute("Producto") Producto producto) {
+	public ModelAndView modificarConsejoSalud(@Valid @ModelAttribute("Producto") Producto producto,BindingResult result) {
 		System.out.println(producto);
 		ModelAndView modelAndView = new ModelAndView("redirect:/producto/productos");
+		if(result.hasErrors()) {
+			modelAndView.setViewName("nuevo_producto");
+			boolean edicion = true;
+			modelAndView.addObject("edicion",edicion);
+			modelAndView.addObject("Producto", producto);
+			
+			return modelAndView;
+		}
+		
 		for(Producto p: productos.getProductos()) {
 			if(p.getCodigo().equals(producto.getCodigo())) {
 				p.setCategoria(producto.getCategoria());
@@ -102,16 +128,5 @@ public class ProductosController {
 	public String volverConsejoSalud() {
 		return "redirect:/producto/productos";
 	}
-	
-	
-	
-	/**Este GetMapping utiliza Model y crea el ArrayList y precarga datos dentro del controlador ProductosController */
-	/*@GetMapping("/productos")
-	public String mostrarProductos(Model model) {
-		List<Producto> productos = new ArrayList<>();
-		productos.add(new Producto("OLYMPIC PERRO ADULTO 19KG","Alimento canino adulto",0001,50,5750.00));
-		productos.add(new Producto("COMPLETE PERRO CACHORRO RAZA PEQUEÑA 20 KG","Alimento canino cachorro",0002,25,10837.00));
-		model.addAttribute("productos", productos);
-		return "productos";
-	}*/
+
 }
--- TP2_Grupo1/src/main/java/ar/edu/unju/fi/listas/ProductosList.java
+++ TP2_Grupo1/src/main/java/ar/edu/unju/fi/listas/ProductosList.java
@@ -3,11 +3,17 @@ package ar.edu.unju.fi.listas;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import ar.edu.unju.fi.model.Producto;
 
+@Component
 public class ProductosList {
 	private List<Producto> productos;
 	
+	/**
+	 * constructor que carga una lista con objetos de tipo productos 
+	 */
 	public ProductosList() {
 		productos = new ArrayList<Producto>();
 		productos.add(new Producto("OLYMPIC PERRO ADULTO 19KG","Alimento canino",50,5750.00,"../images/producto1.jpg",1));
@@ -15,6 +21,7 @@ public class ProductosList {
 		productos.add(new Producto("OLYMPIC PERRO ADULTO 19KG","Alimento canino",0,5750.00,"../images/producto1.jpg",3));
 	}
 	
+	//getter and setter
 	public List<Producto> getProductos() {
 		return productos;
 	}
