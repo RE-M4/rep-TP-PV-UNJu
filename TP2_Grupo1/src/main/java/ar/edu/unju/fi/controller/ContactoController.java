bcfa755f6fa5fdd35aa010fe9ae98db734cc08a2
Javier <Javier@DESKTOP-6AIS6L0>
2023-06-06 00:39:20 -0300

    .

--- TP2_Grupo1/src/main/java/ar/edu/unju/fi/controller/ContactoController.java
+++ TP2_Grupo1/src/main/java/ar/edu/unju/fi/controller/ContactoController.java
@@ -17,22 +17,26 @@ import ar.edu.unju.fi.listas.ListaContacto;
 import ar.edu.unju.fi.controller.ContactoController;
 
 import ar.edu.unju.fi.model.Contacto;
-
+import ar.edu.unju.fi.service.IContactoService;
 import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/contacto")
 public class ContactoController {
-	
+	@Autowired
+	private IContactoService contactoservice;
+	/*ahora hay q conectarlo con el servicio.
 	@Autowired
 	ListaContacto listaMensajes;
-	
+	*/
+	/* copiado y pegado en ContactoServiceImp.java
 	@Autowired
 	Contacto contacto;
-	
+	*/
+	/* reemplazar contacto por contactoService.getMensaje()*/
 	@GetMapping("/listaMensaje")
 	public String contacto(Model model) {
-		model.addAttribute("contacto", listaMensajes.getListaMensaje());
+		model.addAttribute("contacto", contactoservice.getlistaMensajes());
 		return "listaMensaje";
 	}
 	
@@ -42,25 +46,23 @@ public class ContactoController {
             model.addAttribute("Contacto",  contacto);
             return "contacto";
         }
-
+        
         System.out.println(contacto);
-        listaMensajes.getListaMensaje().add(contacto); 
-        model.addAttribute("listaMensajes" , listaMensajes.getListaMensaje());
+        contactoservice.getlistaMensajes().add(contacto); 
+        model.addAttribute("listaMensajes" , contactoservice.getlistaMensajes());
     return "redirect:/contacto/listaMensaje";
-    }
-	
-	 
+    } 
 	@GetMapping("/contacto")
 	public String getContacto(Model model ) {
 		model.addAttribute("Contacto", new Contacto());
 		return "contacto";
 	}
-	
 	@GetMapping("/eliminar_mensaje/{nombreMensaje}")
     public String eliminarMensaje(Model model, @PathVariable(value="nombreMensaje")String nombreMensaje) {
-	for (Contacto contacto  : listaMensajes.getListaMensaje()) {
+		/*metodo buscar*/
+	for (Contacto contacto  : contactoservice.getlistaMensajes()) {
 		if (contacto.getNombre().equals(nombreMensaje)) {
-				listaMensajes.getListaMensaje().remove(contacto);
+			contactoservice.getlistaMensajes().remove(contacto);
 			break;
 		}
 	}
