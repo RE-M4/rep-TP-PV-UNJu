6aea73711739fe605372163c76049bdbb3b259c9
mmarcoschambi <marcos02ch@gmail.com>
2023-06-25 01:25:25 -0300

    Documentación de la clase ISucursalRepository
     La documentación de la clase ISucursalRepository para proporcionar una comprensión más clara de su propósito y funcionalidad

--- TP2_Grupo1/src/main/java/ar/edu/unju/fi/repository/ISucursalRepository.java
+++ TP2_Grupo1/src/main/java/ar/edu/unju/fi/repository/ISucursalRepository.java
@@ -8,6 +8,11 @@ import java.util.List;
 
 @Repository
 public interface ISucursalRepository extends CrudRepository<Sucursal,Long> {
+ /**
+  * Metodo que permite buscar una sucursal segun su estado
+	* @param estado - estado de la sucursal a buscar
+	* @return lista de sucursales que coinciden con el estado
+	*/
 	public List<Sucursal> findByEstado(boolean estado);
 
 }
