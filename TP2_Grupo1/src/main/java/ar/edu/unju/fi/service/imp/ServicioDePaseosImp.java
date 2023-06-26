ebed8542c0a0fccf4b8da655efb7a42dcfec543d
Mauro Levi Rendón <88637963+RE-M4@users.noreply.github.com>
2023-06-25 23:57:42 -0300

    Corrección de errores de la Implementación de Servicios

--- TP2_Grupo1/src/main/java/ar/edu/unju/fi/service/imp/ServicioDePaseosImp.java
+++ TP2_Grupo1/src/main/java/ar/edu/unju/fi/service/imp/ServicioDePaseosImp.java
@@ -47,16 +47,16 @@ public class ServicioDePaseosImp implements IServicioDePaseosService{
 	/**
 	 * Método que recibe un String, busca un objeto en particular y lo devuelve
 	 */
-	public ServicioDePaseo buscarServicio (String nombreBuscado) {
+	/*public ServicioDePaseo buscarServicio (String nombreBuscado) {
 		ServicioDePaseo servicioEncontrado = servicioDePaseo;
-		for(ServicioDePaseo servicio : serviciosLista.getServiciosDePaseo()) {/*Se busca un objeto dentro del Array tienendo en cuenta el nombre*/
+		for(ServicioDePaseo servicio : serviciosLista.getServiciosDePaseo()) {Se busca un objeto dentro del Array tienendo en cuenta el nombre
 			if(servicio.getPaseador().equals(nombreBuscado)) {
 				servicioEncontrado = servicio;
 				break;
 			}
 		}
 		return servicioEncontrado;
-	}
+	}*/
 	
 	/**
 	 * Método que recibe un objeto en particular con datos cambiados, lo busca en 
@@ -64,24 +64,25 @@ public class ServicioDePaseosImp implements IServicioDePaseosService{
 	 * por los que llegan como parámetro.
 	 */
 	public void modificarServicio(ServicioDePaseo servicioAModificar) {
-		for(ServicioDePaseo servicioOriginal : serviciosLista.getServiciosDePaseo()) {
+		/*for(ServicioDePaseo servicioOriginal : serviciosLista.getServiciosDePaseo()) {
 			if(servicioOriginal.getPaseador().equals(servicioAModificar.getPaseador())) {
-				//servicioOriginal = servicioModificado /*Esta línea no modifica los datos de un objeto con otro objeto, solo crea 2 punteros al mismo objeto*/
-				servicioOriginal.setPaseador(servicioAModificar.getPaseador()); /*Al momento de querer cambiar este atributo, queda como está porque sirve como identificador, se debe usar un identificador único que no pueda ser modificado*/
+				//servicioOriginal = servicioModificado Esta línea no modifica los datos de un objeto con otro objeto, solo crea 2 punteros al mismo objeto
+				servicioOriginal.setPaseador(servicioAModificar.getPaseador()); Al momento de querer cambiar este atributo, queda como está porque sirve como identificador, se debe usar un identificador único que no pueda ser modificado
 				servicioOriginal.setDia(servicioAModificar.getDia());
 				servicioOriginal.setHorario(servicioAModificar.getHorario());
 				break;
 			}
-		}
+		}*/
+		
 	}
 	
 	/**
 	 * Método que recibe un objeto en particular, lo busca en el Array y, de encontrarlo, lo elimina.
 	 */
-	public void borrarServicio(String servicioABorrar) {
+	/*public void borrarServicio(String servicioABorrar) {
 		ServicioDePaseo servicioEncontrado = buscarServicio(servicioABorrar);
 		serviciosLista.getServiciosDePaseo().remove(servicioEncontrado);
-	}
+	}*/
 
 	/**
 	 * Método que recibe un objeto nuevo y lo guarda en el Array de objetos
