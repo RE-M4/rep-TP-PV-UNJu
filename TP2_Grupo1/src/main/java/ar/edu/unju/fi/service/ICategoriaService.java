5c7b8c4c893772d80868f04d8d443ae9bb0928ed
DESKTOP-OQ78GJK\Elias <elias3847040@gmail.com>
2023-06-25 21:02:57 -0300

    clase Categoria, Interface ICategoriaRepository, CategoriaServiceMysql

---
+++ TP2_Grupo1/src/main/java/ar/edu/unju/fi/service/ICategoriaService.java
@@ -0,0 +1,11 @@
+package ar.edu.unju.fi.service;
+
+import java.util.List;
+
+import ar.edu.unju.fi.entity.Categoria;
+
+public interface ICategoriaService {
+	
+	public List<Categoria> getListaCategorias();
+
+}
